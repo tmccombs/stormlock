7897359f2898164774de9a8f5c8c1a455da6b64d
Thayne McCombs <astrothayne@gmail.com>
2023-08-26 01:22:12 -0600

    Add comment referencing protobuf types bug
    See: https://github.com/protocolbuffers/protobuf/issues/11402

--- scripts/gen-code.sh
+++ scripts/gen-code.sh
@@ -7,4 +7,5 @@ PROTO_DIR=stormlock/backends/etcd/proto
 python3 -m grpc_tools.protoc --proto_path=. --python_out=. --pyi_out=. $PROTO_DIR/kv.proto $PROTO_DIR/rpc.proto
 python3 -m grpc_tools.protoc --proto_path=. --grpc_python_out=. $PROTO_DIR/rpc.proto
 
+# See https://github.com/protocolbuffers/protobuf/issues/11402
 sed -i -E  -e 's/__slots__ = \[(.*)\]/__slots__ = (\1)/' $PROTO_DIR/*.pyi
